c7d4cc34636451a5a97428f2c43e6b592add98c1
Charles Cantrell <charles.e.cantrell@aexp.com>
2024-12-02 09:04:13 -0800

    runs prettier

--- 2024/Day_1/index.js
+++ 2024/Day_1/index.js
@@ -1,20 +1,22 @@
-const { readFile } = require('node:fs/promises');
+const { readFile } = require("node:fs/promises");
 
 const main = async () => {
   try {
     // Read the file
-    const data = await readFile(`${__dirname}/input.txt`, 'utf8');
+    const data = await readFile(`${__dirname}/input.txt`, "utf8");
     // create two lists to use for comparing columns
     // also create the counts map for the right side
     const rightCounts = {};
     const leftList = [];
     const rightList = [];
     // split the data read into rows
-    const rows = data.split('\n');
+    const rows = data.split("\n");
     // clean up the string and push the numeric values of the IDs onto each list
     // create counts of each of the values in the right list
     for (const row of rows) {
-      const [leftString, rightString] = row.split(' ').filter((val) => val !== '');
+      const [leftString, rightString] = row
+        .split(" ")
+        .filter((val) => val !== "");
       leftList.push(parseInt(leftString));
       rightList.push(parseInt(rightString));
       rightCounts[rightString] = (rightCounts[rightString] || 0) + 1;
@@ -26,7 +28,7 @@ const main = async () => {
     // Also build the similarity score for the left list
     let totalDifference = 0;
     let similarityScore = 0;
-    for (let i = 0 ; i < rows.length ; i++) {
+    for (let i = 0; i < rows.length; i++) {
       const leftVal = leftList[i];
       const rightVal = rightList[i];
       const rightCount = rightCounts[leftVal.toString()] ?? 0;
@@ -37,6 +39,8 @@ const main = async () => {
   } catch (e) {
     console.error(e);
   }
-}
+};
 
-main().catch((e) => { console.error(e) })
+main().catch((e) => {
+  console.error(e);
+});
